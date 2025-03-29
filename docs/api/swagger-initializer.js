d27976b7db7edad1ea907134ef6bf028eb0e78b3
David Chernykh <92572785+vega2475@users.noreply.github.com>
2025-03-29 19:37:26 +0300

    Update swagger-initializer.js

--- docs/api/swagger-initializer.js
+++ docs/api/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://shao-lin.github.io/Music-dating/api/docs/openapi.yaml",
+    url: "https://shao-lin.github.io/Music-dating/docs/api/openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
