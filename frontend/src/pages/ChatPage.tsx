855c7b7caa0425543a28ee97b37e54109e35afd0
Shao-Lin <baulin2005@bk.ru>
2025-06-04 00:47:33 +0300

    change urls

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -80,8 +80,8 @@ export const ChatPage = () => {
     const client = new Client({
       brokerURL:
         window.location.protocol === "https:"
-          ? "wss://localhost:8088/connect-chat"
-          : "ws://localhost:8088/connect-chat",
+          ? "wss://vibedating.ru/api/connect-chat"
+          : "ws://vibedating.ru/api/connect-chat",
 
       reconnectDelay: 5000,
       heartbeatIncoming: 4000,
